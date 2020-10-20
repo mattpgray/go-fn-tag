ea430634d82cbea7b689fab8fa06a2bd13956515
matthew.gray <matthew.gray@insurtechnix.com>
2020-10-20 14:58:46 +0100

    go fmt

--- fns.go
+++ fns.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-	"strings"
 	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
-
-
 type fnTag struct {
-	tag      string
-	node     *ast.Ident
-	pkg      string
-	file     string
-	fn       string
+	tag  string
+	node *ast.Ident
+	pkg  string
+	file string
+	fn   string
 }
 
 func main() {
@@ -31,62 +29,60 @@ func main() {
 	set := token.NewFileSet()
 	packs, err := parser.ParseDir(set, pkg, nil, 0)
 	if err != nil {
-		 fmt.Println("Failed to parse package:", err)
-		 os.Exit(1)
+		fmt.Println("Failed to parse package:", err)
+		os.Exit(1)
 	}
 
 	funcs := []fnTag{}
 	for _, pack := range packs {
-		 for file, f := range pack.Files {
-			  for _, d := range f.Decls {
-					if fn, isFn := d.(*ast.FuncDecl); isFn {
-						node, tag, ok := getFnTag( fn )
-						if ok {
-							funcs = append( funcs, fnTag{ tag:tag, node: node, pkg: pack.Name, file: file, fn: fn.Name.Name } )
-						}
-
+		for file, f := range pack.Files {
+			for _, d := range f.Decls {
+				if fn, isFn := d.(*ast.FuncDecl); isFn {
+					node, tag, ok := getFnTag(fn)
+					if ok {
+						funcs = append(funcs, fnTag{tag: tag, node: node, pkg: pack.Name, file: file, fn: fn.Name.Name})
 					}
-			  }
-		 }
+
+				}
+			}
+		}
 	}
 
 	for _, fn := range funcs {
-		fileName := filepath.Base( fn.file )
+		fileName := filepath.Base(fn.file)
 		extension := filepath.Ext(fileName)
-		fileName = fileName[0:len(fileName)-len(extension)]
-		correct := fmt.Sprintf("%s.%s.%s", fn.pkg, fileName, fn.fn )
+		fileName = fileName[0 : len(fileName)-len(extension)]
+		correct := fmt.Sprintf("%s.%s.%s", fn.pkg, fileName, fn.fn)
 		if fn.tag != correct {
-			fmt.Printf("%s:%d Incorrect fn tag %q. Should be %q\n", fn.file, set.Position( fn.node.Pos() ).Line, fn.tag, correct )
+			fmt.Printf("%s:%d Incorrect fn tag %q. Should be %q\n", fn.file, set.Position(fn.node.Pos()).Line, fn.tag, correct)
 		}
 	}
 }
 
-
-
 func getFnTag(fn *ast.FuncDecl) (*ast.Ident, string, bool) {
 	block := fn.Body
 	for _, stmt := range block.List {
-		ass, ok := stmt.(*ast.AssignStmt )
+		ass, ok := stmt.(*ast.AssignStmt)
 		if !ok {
 			continue
 		}
 
-		if len( ass.Lhs ) != 1 || len( ass.Rhs ) != 1 {
+		if len(ass.Lhs) != 1 || len(ass.Rhs) != 1 {
 			continue
 		}
 
-		variable, ok := ass.Lhs[ 0 ].(*ast.Ident)
+		variable, ok := ass.Lhs[0].(*ast.Ident)
 		if !ok {
 			continue
 		}
 
 		if variable.Name == "fn" {
-			val, ok := ass.Rhs[ 0 ].(*ast.BasicLit)
+			val, ok := ass.Rhs[0].(*ast.BasicLit)
 			if !ok {
 				continue
 			}
 
-			return variable, strings.Trim( val.Value, `"` ), true
+			return variable, strings.Trim(val.Value, `"`), true
 		}
 	}
 	return nil, "", false
